e539bc96826180484efc5814b333750e88c4c52d
Ravinder Singh <42645016+rsnsingh@users.noreply.github.com>
2023-10-27 16:18:45 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ public class Main {
         names.add("Boby");
         names.add("Charan");
 
+        //============= Using Sort Function in Collection Package =============
         System.out.println("Before sorting: " + names);
 
         Collections.sort(names);
@@ -18,5 +19,24 @@ public class Main {
         Collections.sort(names, Collections.reverseOrder());
 
         System.out.println("After sorting in Reverse Order : " + names);
+
+        // ========== Sorting names with Bubble sort ========================
+        names.add("Zaplin");
+        names.add("Dalbir");
+        names.add("Karan");
+        names.add("Varinder");
+        names.add("Atul");
+        for (int i = 0; i < names.size() - 1; i++) {
+            for (int j = 0; j < names.size() - i - 1; j++) {
+                if (names.get(j).compareTo(names.get(j + 1)) > 0) {
+                    String temp = names.get(j);
+                    names.set(j, names.get(j + 1));
+                    names.set(j + 1, temp);
+                }
+            }
+        }
+
+        System.out.println("sorting USING Bubble Sort  : " + names);
+
     }
 }
